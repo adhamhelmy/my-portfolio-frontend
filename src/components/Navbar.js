532c730026bcdd07b4650ae557ead7a5db9301ec
adhamhelmy <adhamhelmy68@icloud.com>
2022-10-11 15:53:50 +0200

    resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 import  React  from "react"
+import pdf from './adham-helmy.pdf'
 import { render } from "react-dom"
 const Navbar = () => {
     
@@ -16,6 +17,11 @@ const Navbar = () => {
                 <Link to={"/projects"} className="nav-link" style={{color:"white"}}>
                     Projects
                 </Link>
+                </li>
+                <li className="nav-item">
+                <a href={pdf} className="nav-link" style={{color:"white"}} target="_blank" rel="noreferrer">
+                    Resume
+                </a>
                 </li>
                 <li className="nav-item" >            
                 <Link to={"/login"} className="nav-link" style={{color:"white"}}>
